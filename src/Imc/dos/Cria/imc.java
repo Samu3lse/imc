14dfbdfcbd3c5d68bb01e2134ad5698a7207b39e
Samu3lse <111393398+Samu3lse@users.noreply.github.com>
2024-01-19 11:14:10 -0300

    Update imc.java

--- src/Imc/dos/Cria/imc.java
+++ src/Imc/dos/Cria/imc.java
@@ -19,7 +19,7 @@ public class imc {
 
         if (imc < 18.5) {
             System.out.println("Magreza : Obesidade grau : 0");
-        } else if (imc >= 18.5 && imc <=24.4) {
+        } else if (imc >= 18.5 && imc <=24.9) {
             System.out.println("Normal : Obesidade grau : 0");
 
         } else if (imc >= 25.0 && imc <= 29.9) {
